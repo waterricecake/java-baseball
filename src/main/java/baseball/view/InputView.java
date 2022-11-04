aa1d81eb519cb6be530e451c6cb3da4ebe250ee9
watterricecake <fruturum@nate.com>
2022-11-04 16:34:15 +0900

    feat: input, outputview 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,15 @@
+package baseball.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String inputView(Scanner scanner){
+        System.out.println("숫자를 입력해주세요 : ");
+        return scanner.nextLine();
+    }
+
+    public static String inputRestartView(Scanner scanner){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return scanner.next();
+    }
+}
