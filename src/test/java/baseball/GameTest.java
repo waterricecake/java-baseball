6348780f23db5108b236c2f033ddb9b667ad9838
watterricecake <fruturum@nate.com>
2022-11-05 19:20:47 +0900

    feat: makeRandomNumbersTest

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,10 +1,17 @@
 package baseball;
 
+import baseball.game.Game;
+import org.assertj.core.util.Sets;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.Set;
+
 import static baseball.game.Game.*;
+import static baseball.game.Options.COUNT_BALL;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class GameTest {
 
     @Test
@@ -12,6 +19,13 @@ public class GameTest {
         String input = "123";
         setUserNumber(input);
         assertThat(userNumbers).containsExactly(1,2,3);
+    }
+
+    @Test
+    void makeRandomNumbersTest(){
+        Game game = new Game();
+        Set<Integer> result = Sets.newHashSet(computerNumbers);
+        assertThat(result.size()).isEqualTo(COUNT_BALL);
     }
 
 }
