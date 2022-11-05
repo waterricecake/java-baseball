be88e5eb03a5427ae33d7615659cc9822c4f3974
watterricecake <fruturum@nate.com>
2022-11-05 19:37:19 +0900

    feat: catchSizeExceptionTest, catchOutOfRangeExceptionTest, catchWrongOrderRestartExceptionTest

---
+++ src/test/java/baseball/ExceptionTest.java
@@ -0,0 +1,42 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ExceptionTest extends NsTest {
+    @Test
+    void catchSizeExceptionTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void catchOutOfRangeExceptionTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("012"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void catchWrongOrderRestartExceptionTest() {
+        assertRandomNumberInRangeTest(
+                () ->
+                {
+                    assertThatThrownBy(()->run("123","3"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1,2,3
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
